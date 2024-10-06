7f21b0d0c8b3cced1d3e2aa334fcfd71be25ddea
itschool <itschool@tou.edu.kz>
2024-10-07 00:43:03 +0500

    init

---
+++ js/main.js
@@ -0,0 +1,63 @@
+let a = 2, b = 3;
+
+// let c = a + b;
+// console.log(c);
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+// console.log((a + b) * a / b ** 2);
+
+// a = a + 10;
+// console.log(a);
+
+// a += 10;
+// console.log(a);
+
+// a *= 6;
+// console.log(a);
+
+// a = a + 1;
+// a += 1;
+
+// console.log(a); // 2
+// a--;
+// console.log(a); // 1
+
+// a = a - 1;
+// a -= 1;
+// a--;
+
+// console.log(a); // 2
+// console.log(a++); // 2 (сперва используем, потом меняем)
+// console.log(a); // 3
+
+// console.log(a); // 2
+// console.log(++a); // 3 (сперва меняем, потом используем)
+// console.log(a); // 3
+
+// console.log(5 !== "5");
+
+let isAdmin = false;
+let hasAccess = false;
+
+// console.log(`true || true = ${true || true}`);
+// console.log(`true || false = ${true || false}`);
+// console.log(`false || true = ${false || true}`);
+// console.log(`false || false = ${false || false}`);
+
+// console.log(`true && true = ${true && true}`);
+// console.log(`true && false = ${true && false}`);
+// console.log(`false && true = ${false && true}`);
+// console.log(`false && false = ${false && false}`);
+
+// console.log(`!true = ${!true}`);
+// console.log(`!false = ${!false}`);
+
+// !hasAccess;
+
+// console.log(5 > 3 && 3 > 1 || 2 > 1 && 1 < 5);
